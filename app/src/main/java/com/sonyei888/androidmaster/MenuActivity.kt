a80a8fef0d33fc434c8c601e390f9fbb42f7f7ea
LAPTOP-S37N1HI5\yeison <yeisoncalderonpoveda08@gmail.com>
2024-09-14 18:24:15 -0500

    Se añadio los vectores necesarios

--- app/src/main/java/com/sonyei888/androidmaster/MenuActivity.kt
+++ app/src/main/java/com/sonyei888/androidmaster/MenuActivity.kt
@@ -5,14 +5,18 @@ import android.os.Bundle
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.sonyei888.androidmaster.firstapp.FirstAppActivity
+import com.sonyei888.androidmaster.imccalculator.ImcCalculatorActivity
 
 class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         setContentView(R.layout.activity_menu)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -20,12 +24,22 @@ class MenuActivity : AppCompatActivity() {
             insets
         }
 
+        window.statusBarColor = ContextCompat.getColor(this, R.color.purple_500)
+
+
         val btnSaludApp = findViewById<Button>(R.id.btnSaludApp)
-        btnSaludApp.setOnClickListener{navigateToSaludApp()}
+        val btnImcApp = findViewById<Button>(R.id.btnIMCApp)
+        btnSaludApp.setOnClickListener { navigateToSaludApp() }
+        btnImcApp.setOnClickListener { navigateToImcApp() }
 
     }
 
-    private fun navigateToSaludApp(){
+    private fun navigateToImcApp() {
+        val intent = Intent(this, ImcCalculatorActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun navigateToSaludApp() {
         val intent = Intent(this, FirstAppActivity::class.java)
         startActivity(intent)
     }
---
+++ app/src/main/java/com/sonyei888/androidmaster/imccalculator/ImcCalculatorActivity.kt
@@ -0,0 +1,23 @@
+package com.sonyei888.androidmaster.imccalculator
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.sonyei888.androidmaster.R
+
+class ImcCalculatorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_imc_calculator)
+        window.statusBarColor = ContextCompat.getColor(this, R.color.purple_500)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
